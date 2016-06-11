3070a807f13f90b0c78cc950edc0706d81277df5
Luka Kalinovcic <kalinovcic@gmail.com>
2016-06-11 23:21:29 +0200

    Small set/map, v0

---
+++ lib/container/small_map.h
@@ -0,0 +1,20 @@
+// Incomplete
+
+template<typename K, typename V> 
+class SmallMap {
+ public:
+  V& operator[](const K& key) {
+    for (int i = 0; i < data_.size(); ++i) {
+      if (data_[i].first == key) return data_[i].second;
+    }
+    data_.push_back(pair<K, V>(key, V()));
+    return data_.back().second;
+  }
+  void clear() {
+    data_.clear();
+  }
+  typename vector<pair<K, V>>::iterator begin() { return data_.begin(); } 
+  typename vector<pair<K, V>>::iterator end() { return data_.end(); } 
+ private:
+  vector<pair<K, V>> data_;
+};
---
+++ lib/container/small_set.h
@@ -0,0 +1,19 @@
+// Incomplete
+
+template<typename T> 
+class SmallSet {
+ public:
+  void insert(const T& value) {
+    for (int i = 0; i < data_.size(); ++i) {
+      if (data_[i] == value) return;
+    }
+    data_.push_back(value);
+  }
+  void clear() {
+    data_.clear();
+  }
+  typename vector<T>::iterator begin() { return data_.begin(); } 
+  typename vector<T>::iterator end() { return data_.end(); } 
+ private:
+  vector<T> data_;
+};
