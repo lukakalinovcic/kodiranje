094e2fc1b0403c9c1ec182943f9edcaf4841425f
Luka Kalinovcic <kalinovcic@gmail.com>
2016-06-19 17:09:41 +0200

    Topological sort updated.

--- lib/graph/topological_sort.h
+++ lib/graph/topological_sort.h
@@ -7,9 +7,11 @@
 
 #include "lib/base/check.h"
 
+namespace graph {
+
 template <typename Graph>
 std::vector<int> TopologicalSort(const Graph& graph) {
-  typedef typename Graph::arc_container::const_iterator const_arc_iterator;
+  typedef typename Graph::out_arc_container::const_iterator const_arc_iterator;
   enum Color { kWhite, kGrey, kBlack };
   std::vector<Color> col(graph.num_nodes(), kWhite);
   std::vector<int> result;
@@ -20,10 +22,10 @@ std::vector<int> TopologicalSort(const Graph& graph) {
       continue;
     }
     col[i] = kGrey;
-    stk.push(std::make_pair(i, graph.node(i).arcs().begin()));
+    stk.push(std::make_pair(i, graph.out_arcs(i).begin()));
     while (!stk.empty()) {
       int u = stk.top().first;
-      if (stk.top().second == graph.node(u).arcs().end()) {
+      if (stk.top().second == graph.out_arcs(u).end()) {
         col[u] = kBlack;
         stk.pop();
         result.push_back(u);
@@ -32,7 +34,7 @@ std::vector<int> TopologicalSort(const Graph& graph) {
       int v = (stk.top().second++)->endpoint();
       if (col[v] == kWhite) {
         col[v] = kGrey;
-        stk.push(std::make_pair(v, graph.node(v).arcs().begin()));
+        stk.push(std::make_pair(v, graph.out_arcs(v).begin()));
       } else if (col[v] == kGrey) {
         result.clear();  // Not a DAG!
         return result;
@@ -42,5 +44,7 @@ std::vector<int> TopologicalSort(const Graph& graph) {
   std::reverse(result.begin(), result.end());
   return result;
 }
+
+}  // namespace graph
 
 #endif  // LIB_GRAPH_TOPOLOGICAL_SORT_H_
